c1fb1083b98ccbaee824d7c1c89fdcf651890a80
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-08-19 21:43:55 +0000

    Adding icon as resource

--- LcmsSpectator/Properties/Resources.Designer.cs
+++ LcmsSpectator/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace LcmsSpectator.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon iconSmall {
+            get {
+                object obj = ResourceManager.GetObject("iconSmall", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
