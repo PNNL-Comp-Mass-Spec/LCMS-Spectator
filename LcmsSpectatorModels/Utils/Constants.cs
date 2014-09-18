f757f3c3638ae64252bc3630aa09a8bc5ea5af26
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-09-18 18:39:27 +0000

    Adding Constants.cs

---
+++ LcmsSpectatorModels/Utils/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace LcmsSpectatorModels.Utils
+{
+    public class Constants
+    {
+        public const int MinCharge = 1;
+        public const int MaxCharge = 15;
+        public const int IsotopeOffsetTolerance = 2;
+    }
+}
