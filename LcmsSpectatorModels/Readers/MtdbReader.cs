46b58580039a0ccab5cb01d9fbea8028901d836e
Michael Degan <michael.degan@pnnl.gov>
2014-10-23 16:18:51 +0000

    Updating MtdbReader to preserve protein information and display the correct peptide sequence.

--- LcmsSpectatorModels/Readers/MtdbReader.cs
+++ LcmsSpectatorModels/Readers/MtdbReader.cs
@@ -39,37 +39,44 @@ namespace LcmsSpectatorModels.Readers
 			{
 				foreach (var id in target.Evidences)
 				{
+                    // Degan: attempting to make it recognize the proteins from .mtdb format
+				    foreach (var prot in id.Proteins)
+				    {
+				        string strippedSequence = target.Sequence;
+				        strippedSequence = strippedSequence.Remove(0, 2);
+				        strippedSequence = strippedSequence.Remove(strippedSequence.Length - 2, 2);
+				        PrSm entry = new PrSm();
+				        entry.Sequence = new Sequence(strippedSequence, new AminoAcidSet());
 
-					string strippedSequence = target.Sequence;
-					strippedSequence = strippedSequence.Remove(0, 2);
-					strippedSequence = strippedSequence.Remove(strippedSequence.Length - 2, 2);
-					PrSm entry = new PrSm();
-					entry.Sequence = new Sequence(strippedSequence, new AminoAcidSet());
+				        string rawSequence = target.Sequence;
+				        int offset = target.Sequence.IndexOf('.');
+				        int length = target.Sequence.Length;
 
-					string rawSequence = target.Sequence;
-					int offset = target.Sequence.IndexOf('.');
-					int length = target.Sequence.Length;
-					
-					foreach (var ptm in id.Ptms)
-					{
-						var position = rawSequence.Length - (length - (ptm.Location + offset));
-						string symbol = "";
-						// We only need to add the sign on positive values - the '-' is automatic on negative values
-						if (ptm.Mass >= 0)
-						{
-							symbol = "+";
-						}
-						rawSequence = rawSequence.Insert(position + 1, symbol + ptm.Mass);
+				        foreach (var ptm in id.Ptms)
+				        {
+				            var position = rawSequence.Length - (length - (ptm.Location + offset));
+				            string symbol = "";
+				            // We only need to add the sign on positive values - the '-' is automatic on negative values
+				            if (ptm.Mass >= 0)
+				            {
+				                symbol = "+";
+				            }
+				            rawSequence = rawSequence.Insert(position + 1, symbol + ptm.Mass);
 
-						Composition modComposition = Composition.ParseFromPlainString(ptm.Formula);
-						Modification mod = Modification.RegisterAndGetModification(ptm.Name, modComposition);
-						entry.Sequence[ptm.Location - 1] = new ModifiedAminoAcid(entry.Sequence[ptm.Location - 1], mod);
-					}
-					
-					entry.SequenceText = rawSequence;
-					entry.Charge = id.Charge;
-					entry.Scan = id.Scan;
-					tree.Add(entry);
+				            Composition modComposition = Composition.ParseFromPlainString(ptm.Formula);
+				            Modification mod = Modification.RegisterAndGetModification(ptm.Name, modComposition);
+				            entry.Sequence[ptm.Location - 1] = new ModifiedAminoAcid(entry.Sequence[ptm.Location - 1], mod);
+				        }
+
+                        // Degan: attempting to make it recognize the proteins from .mtdb format
+				        entry.ProteinName = prot.ProteinName;
+
+                        // Degan: choosing stripped sequence rather than the raw sequence to exclude prior and successive amino acid
+                        entry.SequenceText = strippedSequence;
+				        entry.Charge = id.Charge;
+				        entry.Scan = id.Scan;
+				        tree.Add(entry);
+				    }
 				}
 			}
 
