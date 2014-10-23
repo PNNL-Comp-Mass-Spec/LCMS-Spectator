fe9a7aef6f6dba401d69b6a2100a9aa57f37a759
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-10-23 18:05:13 +0000

    Adding check for layout.xml file and adding opportunity for user to supply a layout file if it is missing

--- LcmsSpectator/Views/MainWindow.xaml.cs
+++ LcmsSpectator/Views/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using LcmsSpectator.DialogServices;
 using LcmsSpectator.ViewModels;
 using LcmsSpectatorModels.Models;
+using Microsoft.Win32;
 using Xceed.Wpf.AvalonDock.Layout.Serialization;
 using TreeView = System.Windows.Controls.TreeView;
 
@@ -22,17 +24,53 @@ namespace LcmsSpectator.Views
 
         private void Window_OnLoaded(object sender, RoutedEventArgs e)
         {
-            if (File.Exists("layout.xml"))
+            var fileName = "layout.xml";
+            if (File.Exists(fileName)) LoadLayout(fileName);
+            else
+            {
+                var result = MessageBox.Show("Cannot find layout.xml. Would you like to search for it?", "", MessageBoxButton.YesNo,
+                    MessageBoxImage.Exclamation);
+                if (result == MessageBoxResult.Yes)
+                {
+                    var dialog = new OpenFileDialog { DefaultExt = ".xml", Filter = @"XML Layout Files (*.xml)|*.xml" };
+
+                    var openResult = dialog.ShowDialog();
+                    if (openResult == true)
+                    {
+                        fileName = dialog.FileName;
+                        LoadLayout(fileName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Without layout file, LcMsSpectator may not display correctly.", "",
+    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Without layout file, LcMsSpectator may not display correctly.", "",
+                        MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+        }
+
+        private void LoadLayout(string layoutFile)
+        {
+            try
             {
                 var serializer = new XmlLayoutSerializer(AvDock);
-                using (var stream = new StreamReader("layout.xml"))
+                using (var stream = new StreamReader(layoutFile))
                 {
                     serializer.LayoutSerializationCallback += (s, args) =>
                     {
                         args.Content = Application.Current.MainWindow.FindName(args.Model.ContentId);
                     };
                     serializer.Deserialize(stream);
-                }   
+                } 
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not load layout file.", "", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
