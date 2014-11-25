a7f4be502d099bb0ed3bfe4ffa3e44fb22a576b6
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-11-25 17:48:37 +0000

    Adding M/z calculation to FeatureReader

--- LcmsSpectatorModels/Readers/FeatureReader.cs
+++ LcmsSpectatorModels/Readers/FeatureReader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using InformedProteomics.Backend.Data.Biology;
+using InformedProteomics.Backend.Data.Composition;
 using InformedProteomics.Backend.Data.Spectrometry;
 using LcmsSpectatorModels.Models;
 
@@ -44,13 +46,15 @@ namespace LcmsSpectatorModels.Readers
             var abundance = Convert.ToDouble(parts[headers["abundance"]]);
             var score = Convert.ToDouble(parts[headers["score"]]);
             var isotopes = ReadIsotopicEnvelope(parts[headers["isotopic_envelope"]]);
+            var minCharge = Convert.ToInt32(parts[headers["min_charge"]]);
+            var maxCharge = Convert.ToInt32(parts[headers["max_charge"]]);
 
             var minPoint = new FeaturePoint
             {
                 Mass = mass,
                 Scan = Convert.ToInt32(parts[headers["min_scan_num"]]),
-                //Mz = Convert.ToDouble(parts[headers["mz"]]),
-                Charge = Convert.ToInt32(parts[headers["min_charge"]]),
+                Mz = (mass + (minCharge * Constants.Proton)) / minCharge,
+                Charge = minCharge,
                 Abundance = abundance,
                 Score = Convert.ToDouble(parts[headers["score"]]),
                 Isotopes = isotopes,
@@ -59,8 +63,8 @@ namespace LcmsSpectatorModels.Readers
             {
                 Mass = mass,
                 Scan = Convert.ToInt32(parts[headers["max_scan_num"]]),
-                //Mz = Convert.ToDouble(parts[headers["mz"]]),
-                Charge = Convert.ToInt32(parts[headers["max_charge"]]),
+                Mz = (mass + (maxCharge * Constants.Proton)) / maxCharge,
+                Charge = maxCharge,
                 Abundance = abundance,
                 Score = score,
                 Isotopes = isotopes
@@ -76,7 +80,7 @@ namespace LcmsSpectatorModels.Readers
             for (int i = 0; i < isotopePeaks.Length; i++)
             {
                 var parts = isotopePeaks[i].Split(',');
-                if (parts.Length == 2) continue;
+                if (parts.Length < 2) continue;
                 int index;
                 double relativeIntensity;
                 if (!Int32.TryParse(parts[0], out index) || !Double.TryParse(parts[1], out relativeIntensity))
