d75ab9924f58890961d26e6be6d16c5514bbff33
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2015-02-19 22:38:34 +0000

    Adding Modification view model

---
+++ LcmsSpectator/ViewModels/ModificationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InformedProteomics.Backend.Data.Sequence;
+using ReactiveUI;
+
+namespace LcmsSpectator.ViewModels
+{
+    public class ModificationViewModel: ReactiveObject
+    {
+        public ModificationViewModel(Modification modification)
+        {
+            Modification = modification;
+        }
+
+        private Modification _modification;
+        public Modification Modification
+        {
+            get { return _modification; }
+            set { this.RaiseAndSetIfChanged(ref _modification, value); }
+        }
+
+        private bool _selected;
+        public bool Selected
+        {
+            get { return _selected; }
+            set { this.RaiseAndSetIfChanged(ref _selected, value); }
+        }
+    }
+}
