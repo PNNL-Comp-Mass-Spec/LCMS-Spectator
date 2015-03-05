7b24cce80049ae054863c42ec57473a800689b84
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2015-03-05 21:02:48 +0000

    Dms lookup now saves previous searches, and uses autocompletion

--- LcmsSpectator/ViewModels/DmsLookupViewModel.cs
+++ LcmsSpectator/ViewModels/DmsLookupViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive.Linq;
 using LcmsSpectator.DialogServices;
 using LcmsSpectator.Readers;
@@ -11,9 +12,6 @@ namespace LcmsSpectator.ViewModels
     {
         public ReactiveList<DmsDatasetViewModel> Datasets { get; private set; } 
         public ReactiveList<DmsJobViewModel> Jobs { get; private set; } 
-
-        public int NumberOfWeeks { get; set; }
-        public string DatasetFilter { get; set; }
 
         public IReactiveCommand LookupCommand { get; private set; }
         public IReactiveCommand OpenCommand { get; private set; }
@@ -27,7 +25,9 @@ namespace LcmsSpectator.ViewModels
             IsFirstSearch = true;
             Status = false;
             _dialogService = dialogService;
-            NumberOfWeeks = 2;
+            _previousResultsList = new List<Tuple<string, int>>();
+            NumberOfWeeks = 10;
+            PreviousDatasets = new Dictionary<string, int>();
             Datasets = new ReactiveList<DmsDatasetViewModel>();
             Jobs = new ReactiveList<DmsJobViewModel>();
             _dmsLookupUtility = new DmsLookupUtility();
@@ -72,6 +72,14 @@ namespace LcmsSpectator.ViewModels
             this.WhenAnyValue(x => x.SelectedDataset, x => x.IsFirstSearch)
                 .Select(x => (x.Item1 == null || String.IsNullOrEmpty(x.Item1.DatasetFolderPath) && !x.Item2))
                 .ToProperty(this, x => x.IsNoResultsShown, out _isNoResultsShown);
+
+            this.WhenAnyValue(x => x.DatasetFilter)
+                .Where(filter => !String.IsNullOrEmpty(filter))
+                .Where(filter => PreviousDatasets.ContainsKey(filter))
+                .Select(filter => PreviousDatasets[filter])
+                .Subscribe(numWeeks => NumberOfWeeks = numWeeks);
+
+            OpenPreviousResultFile();
         }
 
         private DmsDatasetViewModel _selectedDataset;
@@ -101,6 +109,27 @@ namespace LcmsSpectator.ViewModels
             set { this.RaiseAndSetIfChanged(ref _isFirstSearch, value); }
         }
 
+        private int _numberOfWeeks;
+        public int NumberOfWeeks
+        {
+            get { return _numberOfWeeks; }
+            set { this.RaiseAndSetIfChanged(ref _numberOfWeeks, value); }
+        }
+
+        private string _dataSetFilter;
+        public string DatasetFilter
+        {
+            get { return _dataSetFilter; }
+            set { this.RaiseAndSetIfChanged(ref _dataSetFilter, value); }
+        }
+
+        private Dictionary<string, int> _previousDatasets;
+        public Dictionary<string, int> PreviousDatasets
+        {
+            get { return _previousDatasets; }
+            private set { this.RaiseAndSetIfChanged(ref _previousDatasets, value); }
+        }
+
         /// <summary>
         /// Lookup datasets and jobs for the past NumberOfWeeks with a filter given by DatasetFilter
         /// </summary>
@@ -126,12 +155,48 @@ namespace LcmsSpectator.ViewModels
             if (Datasets.Count > 0) SelectedDataset = Datasets[0];
         }
 
+        private void OpenPreviousResultFile()
+        {
+            var prevResults = new Dictionary<string, int>();
+            if (File.Exists(PreviousResultsFile))
+            {
+                var file = File.ReadAllLines(PreviousResultsFile);
+                foreach (var line in file)
+                {
+                    int numWeeks = 0;
+                    var parts = line.Split('\t');
+                    if (parts.Length < 2) continue;
+                    var dataSetName = parts[0];
+                    if (!Int32.TryParse(parts[1], out numWeeks)) continue;
+                    _previousResultsList.Add(new Tuple<string, int>(dataSetName, numWeeks));
+                    if (!prevResults.ContainsKey(dataSetName)) prevResults.Add(dataSetName, numWeeks);
+                    else prevResults[dataSetName] = numWeeks;
+                }
+            }
+            PreviousDatasets = prevResults;
+        }
+
+        private void WritePreviousResultFile()
+        {
+            var previousResults = _previousResultsList;
+            if (_previousResultsList.Count > 30) previousResults = _previousResultsList.GetRange(0, Math.Min(30, _previousResultsList.Count-1));
+            using (var outFile = new StreamWriter(PreviousResultsFile))
+            {
+                foreach (var item in previousResults)
+                {
+                    outFile.WriteLine("{0}\t{1}", item.Item1, item.Item2);
+                }
+            }
+        }
+
         /// <summary>
         /// Selected data set and close window
         /// </summary>
         private void Open()
         {
             Status = true;
+            _previousResultsList.Insert(0, new Tuple<string, int>(DatasetFilter.Trim(), NumberOfWeeks));
+            WritePreviousResultFile();
             if (ReadyToClose != null) ReadyToClose(this, EventArgs.Empty);
         }
 
@@ -146,5 +211,7 @@ namespace LcmsSpectator.ViewModels
 
         private readonly DmsLookupUtility _dmsLookupUtility;
         private readonly IDialogService _dialogService;
+        private List<Tuple<string, int>> _previousResultsList; 
+        private const string PreviousResultsFile = "dmsSearches.txt";
     }
 }
