573c5a9a92c5f675f1a429f928137b15e47249ff
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2015-03-31 18:59:44 +0000

    Adding IDataSetReader

---
+++ LcmsSpectator/Readers/DataSetReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LcmsSpectator.Models;
+using LcmsSpectator.ViewModels;
+
+namespace LcmsSpectator.Readers
+{
+    public class DataSetReader: IDataSetReader
+    {
+
+        public Task<DataSetViewModel> OpenDataSet(string rawFilePath, string idFilePath, string featureFilePath, Models.ToolType tool = ToolType.MsPathFinder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataSetViewModel> OpenDataSet(DataSetViewModel dsVm, string idFilePath, string featureFilePath, Models.ToolType tool = ToolType.MsPathFinder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataSetViewModel> OpenFromDms(DmsLookupViewModel dmsLookUp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LcmsSpectator/Readers/IDataSetReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using LcmsSpectator.Models;
+using LcmsSpectator.ViewModels;
+
+namespace LcmsSpectator.Readers
+{
+    interface IDataSetReader
+    {
+        Task<DataSetViewModel> OpenDataSet(string rawFilePath, string idFilePath, string featureFilePath, ToolType tool=ToolType.MsPathFinder);
+        Task<DataSetViewModel> OpenDataSet(DataSetViewModel dsVm, string idFilePath, string featureFilePath, ToolType tool=ToolType.MsPathFinder);
+        Task<DataSetViewModel> OpenFromDms(DmsLookupViewModel dmsLookUp);
+    }
+}
