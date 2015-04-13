119284c5a3681f1697d71218607e57fdde3c7799
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2015-04-13 21:39:11 +0000

    Adding DataReader.cs

---
+++ LcmsSpectator/Readers/DataReader.cs
@@ -0,0 +1,172 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataReader.cs" company="Pacific Northwest National Laboratory">
+//   2015 Pacific Northwest National Laboratory
+// </copyright>
+// <author>Christopher Wilkins</author>
+// <summary>
+//   The DataReader is a class that encapsulates reading raw files, ID files, and feature files.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LcmsSpectator.Readers
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using LcmsSpectator.Config;
+    using LcmsSpectator.Models;
+    using LcmsSpectator.Utils;
+    using LcmsSpectator.ViewModels;
+    using ReactiveUI;
+
+    /// <summary>
+    /// The DataReader is a class that encapsulates reading raw files, ID files, and feature files.
+    /// </summary>
+    public class DataReader : ReactiveObject, IDataReader
+    {
+        /// <summary>
+        /// Value indicating whether or not the data reader is currently reading raw files.
+        /// </summary>
+        private bool readingRawFiles;
+
+        /// <summary>
+        /// Value indicating whether or not the data reader is currently reading raw files.
+        /// </summary>
+        private bool readingIdFiles;
+
+        /// <summary>
+        /// Value indicating whether or not the data reader is currently reading feature files.
+        /// </summary>
+        private bool readingFeatureFiles;
+
+        /// <summary>
+        /// Initializes a new instance of the DataReader class. 
+        /// </summary>
+        public DataReader()
+        {
+            this.ReadingRawFiles = false;
+            this.ReadingIdFiles = false;
+            this.ReadingFeatureFiles = false;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading raw files.
+        /// </summary>
+        public bool ReadingRawFiles
+        {
+            get { return this.readingRawFiles; }
+            private set { this.RaiseAndSetIfChanged(ref this.readingRawFiles, value); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading id files.
+        /// </summary>
+        public bool ReadingIdFiles
+        {
+            get { return this.readingIdFiles; }
+            private set { this.RaiseAndSetIfChanged(ref this.readingIdFiles, value); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading feature files.
+        /// </summary>
+        public bool ReadingFeatureFiles
+        {
+            get { return this.readingFeatureFiles; }
+            private set { this.RaiseAndSetIfChanged(ref this.readingFeatureFiles, value); }
+        }
+
+        /// <summary>
+        /// Open and read ID file and add IDs to data set.
+        /// </summary>
+        /// <param name="dataSetViewModel">DataSetViewModel to add IDs to.</param>
+        /// <param name="idFilePath">Path for ID file to read.</param>
+        /// <param name="modIgnoreList">Modifications to ignore in identifications.</param>
+        /// <returns>Task that reads ID file.</returns>
+        public async Task ReadIdFile(DataSetViewModel dataSetViewModel, string idFilePath, IEnumerable<string> modIgnoreList = null)
+        {
+            var reader = IdFileReaderFactory.CreateReader(idFilePath);
+            var ids = await reader.ReadAsync(modIgnoreList);
+            ids.SetLcmsRun(dataSetViewModel.LcMs, dataSetViewModel.Title);
+            dataSetViewModel.ScanViewModel.AddIds(ids.AllPrSms);
+        }
+
+        /// <summary>
+        /// Open a data set given raw file, id file, and feature file.
+        /// </summary>
+        /// <param name="dataSetViewModel">DataSetViewModel to associate open dataset with</param>
+        /// <param name="rawFilePath">Path to raw file to open</param>
+        /// <param name="idFilePath">Path to MS-GF+ or MS-PathFinder results file</param>
+        /// <param name="featureFilePath">Path to feature list file</param>
+        /// <param name="toolType">Type of ID tool used for this data set</param>
+        /// <param name="modIgnoreList">Modifications to ignore if found in ID list.</param>
+        /// <returns>Task that opens the data set.</returns>
+        public async Task OpenDataSet(
+                                    DataSetViewModel dataSetViewModel,
+                                    string rawFilePath,
+                                    string idFilePath = "", 
+                                    string featureFilePath = "", 
+                                    ToolType? toolType = ToolType.MsPathFinder,
+                                    IEnumerable<string> modIgnoreList = null)
+        {
+            // Open raw file, if not already open.
+            if (!string.IsNullOrEmpty(rawFilePath) && dataSetViewModel.LcMs == null)
+            {
+                this.ReadingRawFiles = true;
+                await dataSetViewModel.InitializeAsync(rawFilePath);
+                this.ReadingRawFiles = false;
+            }
+
+            // Show neighboring charge state XICs by default for MSPathFinder results
+            if (toolType != null && toolType == ToolType.MsPathFinder)
+            {
+                dataSetViewModel.XicViewModel.PrecursorViewMode = PrecursorViewMode.Charges;
+            }
+
+            // Open ID file
+            if (!string.IsNullOrEmpty(idFilePath))
+            {
+                this.ReadingIdFiles = true;
+                await this.ReadIdFile(dataSetViewModel, idFilePath, modIgnoreList);
+                this.ReadingIdFiles = false;
+            }
+
+            // Open feature file
+            if (!string.IsNullOrEmpty(featureFilePath))
+            {
+                this.ReadingFeatureFiles = true;
+                dataSetViewModel.FeatureMapViewModel.OpenFeatureFile(featureFilePath);
+                dataSetViewModel.FeatureMapViewModel.UpdateIds(dataSetViewModel.ScanViewModel.FilteredData);
+                this.ReadingRawFiles = false;
+            }
+        }
+
+        /// <summary>
+        /// Get raw files in a given directory.
+        /// This method is not recursive.
+        /// </summary>
+        /// <param name="directoryPath">Path for directory to search for raw files in.</param>
+        /// <param name="datasetName">
+        /// Path of file to search for raw files in the same directory.
+        /// This is the full path.
+        /// </param>
+        /// <returns>IEnumerable of raw file names.</returns>
+        public List<string> GetRawFilesByDataSetName(string directoryPath, string datasetName)
+        {
+            var rawFiles = new List<string>();
+            var directory = Directory.GetFiles(directoryPath);
+            foreach (var filePath in directory)
+            {
+                // Raw file in same directory as tsv file?
+                var fileName = Path.GetFileNameWithoutExtension(filePath);
+                rawFiles.AddRange(from extension in FileConstants.RawFileExtensions 
+                                  where fileName == datasetName && filePath.EndsWith(extension, true, CultureInfo.InvariantCulture) 
+                                  select filePath);
+            }
+
+            return rawFiles;
+        }
+    }
+}
---
+++ LcmsSpectator/Readers/IDataReader.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDataReader.cs" company="Pacific Northwest National Laboratory">
+//   2015 Pacific Northwest National Laboratory
+// </copyright>
+// <author>Christopher Wilkins</author>
+// <summary>
+//   The DataReader is an interface for reading raw files, ID files, and feature files.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LcmsSpectator.Readers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using LcmsSpectator.Models;
+    using LcmsSpectator.ViewModels;
+
+    /// <summary>
+    /// The DataReader is an interface for reading raw files, ID files, and feature files.
+    /// </summary>
+    public interface IDataReader
+    {
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading raw files.
+        /// </summary>
+        bool ReadingRawFiles { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading id files.
+        /// </summary>
+        bool ReadingIdFiles { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the data reader is currently reading feature files.
+        /// </summary>
+        bool ReadingFeatureFiles { get; }
+
+        /// <summary>
+        /// Open and read ID file and add IDs to data set.
+        /// </summary>
+        /// <param name="dataSetViewModel">DataSetViewModel to add IDs to.</param>
+        /// <param name="idFilePath">Path for ID file to read.</param>
+        /// <param name="modIgnoreList">Modifications to ignore in identifications.</param>
+        /// <returns>Task that reads ID file.</returns>
+        Task ReadIdFile(DataSetViewModel dataSetViewModel, string idFilePath, IEnumerable<string> modIgnoreList = null);
+
+        /// <summary>
+        /// Open a data set given raw file, id file, and feature file.
+        /// </summary>
+        /// <param name="dataSetViewModel">DataSetViewModel to associate open dataset with</param>
+        /// <param name="rawFilePath">Path to raw file to open</param>
+        /// <param name="idFilePath">Path to MS-GF+ or MS-PathFinder results file</param>
+        /// <param name="featureFilePath">Path to feature list file</param>
+        /// <param name="toolType">Type of ID tool used for this data set</param>
+        /// <param name="modIgnoreList">Modifications to ignore if found in ID list.</param>
+        /// <returns>Task that opens the data set.</returns>
+        Task OpenDataSet(
+                        DataSetViewModel dataSetViewModel,
+                        string rawFilePath,
+                        string idFilePath = "",
+                        string featureFilePath = "",
+                        ToolType? toolType = ToolType.MsPathFinder,
+                        IEnumerable<string> modIgnoreList = null);
+
+        /// <summary>
+        /// Get raw files in a given directory.
+        /// This method is not recursive.
+        /// </summary>
+        /// <param name="directoryPath">Path for directory to search for raw files in.</param>
+        /// <param name="datasetName">
+        /// Path of file to search for raw files in the same directory.
+        /// This is the full path.
+        /// </param>
+        /// <returns>IEnumerable of raw file names.</returns>
+        List<string> GetRawFilesByDataSetName(string directoryPath, string datasetName);
+    }
+}
