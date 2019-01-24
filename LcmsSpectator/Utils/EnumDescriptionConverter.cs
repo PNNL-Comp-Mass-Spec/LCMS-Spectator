5a11dc63cb308d5ee59af73e17384c2cbab566a3
alchemistmatt <matthew.monroe@pnnl.gov>
2019-01-24 15:34:17 -0800

    Add enum utility classes

---
+++ LcmsSpectator/Utils/EnumDescriptionConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LcmsSpectator.Utils
+{
+    // Extract enum description
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var memberInfos = value.GetType().GetMember(value.ToString());
+
+            if (memberInfos.Length > 0)
+            {
+                var attrs = memberInfos[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attrs.Length > 0)
+                    return ((DescriptionAttribute)attrs[0]).Description;
+            }
+
+            return value;
+
+            // or maybe just
+            //throw new InvalidEnumArgumentException(string.Format("no description found for enum {0}", value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
