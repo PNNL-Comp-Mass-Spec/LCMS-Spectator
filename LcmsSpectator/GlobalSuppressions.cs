b20ed96900567cd5365ac39984d170b85174ea21
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-06 17:16:56 -0700

    Suppress warnings

---
+++ LcmsSpectator/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "RCS1090:Add call to 'ConfigureAwait' (or vice versa).", Justification = "Ignore this suggestion", Scope = "member", Target = "~M:LcmsSpectator.Readers.MsgfSynopsisReader.ReadFile(System.Int32,System.Int32,System.Collections.Generic.IReadOnlyCollection{System.String},System.IProgress{System.Double})~System.Threading.Tasks.Task{System.Collections.Generic.IEnumerable{LcmsSpectator.Models.PrSm}}")]
